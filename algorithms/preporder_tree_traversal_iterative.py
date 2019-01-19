326ad3adbc71d8d242d5c43bb2300d8554e2f565
Anurag Panwar <apanuragpanwar@gmail.com>
2019-01-19 13:08:22 -0800

    Iterative solution for postorder and preorder tree traversal

---
+++ algorithms/preporder_tree_traversal_iterative.py
@@ -0,0 +1,32 @@
+#Example
+#  Input: [1, null, 2, 3]
+#   1
+#     \
+#      2
+#     /
+#    3
+#  Output: [1, 2, 3]
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution(object):
+    def preorder(self, root):
+        res = []
+        stack = [root]
+
+        if root is None:
+            return res
+
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+            if cur.right is not None:
+                stack.append(cur.right)
+            if cur.left is not None:
+                stack.append(cur.left)
+        return res
