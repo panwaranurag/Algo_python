cc9b73d663d918e949a34fece4ea5d7e8c6929db
Anurag Panwar <apanuragpanwar@gmail.com>
2019-01-19 11:48:24 -0800

    Recursive code for postorder and preorder tree traversal

---
+++ algorithms/postorder_tree_traversal_recursive.py
@@ -0,0 +1,26 @@
+#Example
+#  Input: [1, null, 2, 3]
+#   1
+#     \
+#      2
+#     /
+#    3
+#  Output: [1, 2, 3]
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorder(self, root):
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        if root:
+            self.helper(root.left, res)
+            self.helper(root.right, res)
+            res.append(root.val)
